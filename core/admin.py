03d7469b9935f7b551aca80528185716a7f427c0
Alex Lopes <alexlr10@hotmail.com>
2020-03-24 23:27:58 -0300

    model mensalista

--- core/admin.py
+++ core/admin.py
@@ -5,8 +5,12 @@ class MovRotativoAdmin(admin.ModelAdmin):
     list_display = ('checkin','checkout','valor_hora','veiculo',
                     'pago','total','horas_total')
 
+    def veiculo(self,obj):
+        return obj.veiculo
+
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
 admin.site.register(MovRotativo,MovRotativoAdmin)
+admin.site.register(Mensalista)
