6569697901172fad2a624d5ba03d3cbdd20f647a
Alex Lopes <alexlr10@hotmail.com>
2020-03-25 12:49:03 -0300

    creates ok

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,20 @@ from .views import *
 
 urlpatterns = [
     path('', home, name = 'home'),
+
     path('pessoas', lista_pessoas, name = 'lista_pessoas'),
+    path('pessoas_create', pessoa_create, name = 'pessoa_create'),
+
+    path('veiculos', lista_veiculos, name = 'lista_veiculos'),
+    path('veiculos_create', veiculo_create, name = 'veiculo_create'),
+
+    path('movrot', lista_movrotativo, name = 'lista_movrot'),
+    path('movrot_create', movrot_create, name = 'movrot_create'),
+
+    path('mensalista', lista_mensalista, name = 'lista_mensalista'),
+    path('mensalista_create', mensalista_create, name = 'mensalista_create'),
+
+    path('movmensalista', lista_movmensalista, name = 'lista_movmensalista'),
+    path('movmensalista_create', movmensalista_create, name = 'movmensalista_create'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Pessoa
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+
 
 def home(request):
     context = {
@@ -9,4 +11,82 @@ def home(request):
 
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
-    return render (request,'core/lista_pessoas.html',{'pessoas':pessoas})
+    form = PessoaForm()
+    data = {
+        'pessoas':pessoas,
+        'form':form
+    }
+    return render (request,'core/lista_pessoas.html',data)
+
+def pessoa_create(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_pessoas')
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    form = VeiculoForm()
+    data = {
+        'veiculos':veiculos,
+        'form': form
+    }
+
+    return render (request,'core/lista_veiculos.html',data)
+
+def veiculo_create(request):
+    form = VeiculoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_veiculos')
+
+
+
+def lista_movrotativo(request):
+    movrot = MovRotativo.objects.all()
+    form = MovRotativoForm()
+    data = {
+        'movrot': movrot,
+        'form': form
+    }
+    return render(request,'core/lista_movrot.html',data)
+
+def movrot_create(request):
+    form = MovRotativoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_movrot')
+
+
+def lista_mensalista(request):
+    mensalista = Mensalista.objects.all()
+    form = MensalistaForm()
+    data = {
+        'mensalista': mensalista,
+        'form':form
+    }
+    return render(request,'core/lista_mensalista.html',data)
+
+def mensalista_create(request):
+    form = MovRotativoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_mensalista')
+
+
+
+def lista_movmensalista(request):
+    movmensalista = MovMensalista.objects.all()
+    form = MovMensalistaForm()
+    data = {
+        'movmensalista': movmensalista,
+        'form':form
+    }
+    return render(request,'core/lista_movmensalista.html',data)
+
+def movmensalista_create(request):
+    form = MovMensalistaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_movmensalista')
